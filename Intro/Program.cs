869b14a9b177ba892d769dc3d2d851e5b9d3f7c4
Danilo Noguera <danilo.noguera@gmail.com>
2025-03-14 19:26:03 -0600

    Agregar archivos de proyecto.

---
+++ Intro/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 35;
+            Console.WriteLine($"El valor de edad es {edad}");
+        }
+    }
+}
